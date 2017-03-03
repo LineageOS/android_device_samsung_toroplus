5f79484ba88e69710402f345854f63ff46720b30
Andreas Blaesius <skate4life@gmx.de>
2017-03-03 19:14:52 +0100

    toroplus: Rebrand to LineageOS
    Change-Id: Ie5e12bcad9a7a812b3385faf71bf9bf8e29d138e

--- lineage.mk
+++ lineage.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := GN-CDMA
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := toroplus
-PRODUCT_NAME := cm_toroplus
+PRODUCT_NAME := lineage_toroplus
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
